d4329142931db68fd3d4f9fe614f4701f9a21789
Yasin <mustafayasinemir@gmail.com>
2024-03-13 09:31:05 +0300

    db connected

---
+++ eCommerce.Api/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using eCommerce.Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace eCommerce.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
